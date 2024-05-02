f459883b33a9459ca433e754f28ff238d4e308fd
benjaminmillertx <156035893+benjaminmillertx@users.noreply.github.com>
2024-05-02 08:36:59 -0500

    Update 303_2_1.java

--- 303_2_1.java
+++ 303_2_1.java
@@ -39,4 +39,35 @@ public class Homework303_2_1 {
     System.out.println.(div);
     } 
    }
-  
+// Write a program that declares two double variables, assigns a number to each, and divides the larger by the smaller number. Assign the result to a variable. Print out the result. Now, cast the result to an integer. Print out the result //
+   public static void
+main(String[] args) {
+        double num1 =10.5;
+        double num2= 5.3;
+
+        double result = num 1 > num2 ? num1 / num2 : num2 / num1;
+System.out.println("Result before casting to integer: " +
+                   result;
+                         intResult - (int) 
+                    result;
+   System.out.println("result after casting to integer " + o a double and assign it to q. Print q again.
+ 
+                              intResult); 
+            } 
+}
+
+//Write a program that declares two integer variables, x and y, and assigns the number 5 to x and the number 6 to y. Declare a variable q and assign y/x to it and print q. Now, cast y to a double and assign it to q. Print q again.//
+
+public class Main{
+   public static void 
+main(String[] args) {
+        int x = 5;
+        int y = 6;
+
+        int q = y / x;
+System.outprintln(" Value of q after integer division: " + q);
+
+           q=(double) y;
+System.out.println("Value of q after casting y to double:" + q);
+  }
+}
