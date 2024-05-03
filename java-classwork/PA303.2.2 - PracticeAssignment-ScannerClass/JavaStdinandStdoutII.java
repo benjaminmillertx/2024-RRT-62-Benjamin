2303799d2f7ab3f483b91bebc6a04eb59fbe70f0
benjaminmillertx <156035893+benjaminmillertx@users.noreply.github.com>
2024-05-03 05:08:36 -0500

    Create JavaStdinandStdoutII.java

---
+++ java-classwork/PA303.2.2 - PracticeAssignment-ScannerClass/JavaStdinandStdoutII.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = Integer.parseInt(scan.nextLine());
+        double d = Double.parseDouble(scan.nextLine());
+        String s =scan.nextLine();
+
+        // String,Double,Int 
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
+
+
+
+
