b43834eb466466ab1d94ab161df36f9efe59401c
benjaminmillertx <156035893+benjaminmillertx@users.noreply.github.com>
2024-05-05 20:22:00 -0500

    Create compass.java

---
+++ practice application/compass.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Compass{
+ public static void 
+main(String [] args) {
+     Scanner scanner = new 
+Scanner(System.in);
+
+System.out.println("Creating a compass..") 
+
+   // Magenetize the needle 
+
+  String needle="N";
+
+System.out.println("Rubbing the needle with a magnet to magnetize it.);
+
+//Assemble the compass
+
+String cork ="C";
+
+//Fill the bowl bowl with water
+System.out.println("Filling the bowl with water...);
+
+//Place the cork in water
+System.out.println("Placing the cork with the needle on the water surface...")
+
+//Wait for needle to settle
+System.out.println("Waiting for the needle to settle..");
+
+//Display compass orientation
+System.out.println("The needle is pointing towards the Earth's North Pole.")
+
+System.out.println("Congratulations! You have created a homemade compass.");
+       }
+}
