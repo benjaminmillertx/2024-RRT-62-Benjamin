3f580cea10ec8545fde1afd3d410cd487db49b0d
benjaminmillertx <156035893+benjaminmillertx@users.noreply.github.com>
2024-05-09 07:07:43 -0500

    Create homework303.9.2.java

---
+++ java-classwork/GLAB303.9.2 _Basic_Inheritance_Example/homework303.9.2.java
@@ -0,0 +1,22 @@
+
+public class Doctor {
+   String DoctorName;
+   String Department;
+  public void Doctor_Details() {
+       System.out.println("Doctor Details...");
+   }
+}
+
+public class Surgeon extends Doctor {
+   void Surgeon_Details() {
+       System.out.println("Surgeon  Detail...");
+       System.out.println(Department = "Cardio");
+   }
+}
+public class Surgeon extends Doctor {
+   void Surgeon_Details() {
+       System.out.println("Surgeon  Detail...");
+       System.out.println(Department = "Cardio");
+   }
+}
+
