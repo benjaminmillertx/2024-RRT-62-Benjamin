c7bce3ef3aa12dffde5cd6e93aadd5b407da3cd3
benjaminmillertx <156035893+benjaminmillertx@users.noreply.github.com>
2024-05-09 13:41:12 -0500

    Create SBA1Practicesolution.java

---
+++ java-classwork/SBA1Practicesolution.java
@@ -0,0 +1,50 @@
+package org.example;
+
+public class SBA1Practice{
+
+public static void
+main(String[] args){
+String s="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//part 1
+
+for(int i=0; i < s.length(); i++)
+  }
+
+System.out.println(s.charAt(i));
+//part 2
+String ten"10";
+String hundred= "100";
+int invValueOfTen=Integer.valueOf(ten);
+int intValueOfHundred=Integer.valueOf(hundred);
+
+System.out.println(intValueOfTen); //Output:10
+
+System.out.println(intValueOfHundred); // Output:100
+
+//part 3
+//Given an integer array find the sum of all the integers in the array
+int[] array={2,4,5,6,7,8,10};
+int sum = 0;
+for(int num:array)
+{
+  sum+= num;
+}
+System.out.println("Sum of array elements: " + sum); 
+//Output: Sum of array elements:42
+
+//part4
+//given the array of string convert them to numbers and print the sum
+string[] nums+{"2","4","5","6","7","8","10"};
+
+int sumOfStringNums=0;
+
+for(String num: nums) {
+  {
+  sumOfStringNums += Integer.valueOf(num);
+  }
+
+  System.out.println("Sum of string numbers:" +
+  sumOfStringNums); // Output: Sum of string numbers:42}
+}
+
+
