379757e416fd05409d310feecb8790a01e55cb4d
benjaminmillertx <156035893+benjaminmillertx@users.noreply.github.com>
2024-05-14 12:06:30 -0500

    Update example5.java

--- java-classwork/Java Collection: HashMap Exercises [12 exercises with solution]/example5.java
+++ java-classwork/Java Collection: HashMap Exercises [12 exercises with solution]/example5.java
@@ -1,1 +1,22 @@
  Write a Java program to remove all mappings from a map.
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample {
+    public static void main(String[] args) {
+        // Creating a map
+        Map<String, Integer> map = new HashMap<>();
+        
+        // Adding some entries
+        map.put("A", 1);
+        map.put("B", 2);
+        map.put("C", 3);
+        
+        System.out.println("Map before removal: " + map);
+        
+        // Removing all mappings
+        map.clear();
+        
+        System.out.println("Map after removal: " + map);
+    }
+}
