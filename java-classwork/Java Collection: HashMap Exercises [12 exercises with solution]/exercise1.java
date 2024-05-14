4d0ab86b11cb47f44665f408ec32b663bb6d5187
benjaminmillertx <156035893+benjaminmillertx@users.noreply.github.com>
2024-05-14 10:47:41 -0500

    Update exercise1.java

--- java-classwork/Java Collection: HashMap Exercises [12 exercises with solution]/exercise1.java
+++ java-classwork/Java Collection: HashMap Exercises [12 exercises with solution]/exercise1.java
@@ -1,1 +1,17 @@
 Write a Java program to associate the specified value with the specified key in a HashMap.
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        // Creating a HashMap
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        // Associating a value with a key
+        hashMap.put("key1", 10);
+        hashMap.put("key2", 20);
+        hashMap.put("key3", 30);
+
+        // Printing the HashMap
+        System.out.println("HashMap: " + hashMap);
+    }
+}
