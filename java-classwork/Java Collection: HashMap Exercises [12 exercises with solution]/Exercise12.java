c5e6f87d406d560f25b96471a0bc6a627a782975
benjaminmillertx <156035893+benjaminmillertx@users.noreply.github.com>
2024-05-14 12:49:02 -0500

    Update Exercise12.java

--- java-classwork/Java Collection: HashMap Exercises [12 exercises with solution]/Exercise12.java
+++ java-classwork/Java Collection: HashMap Exercises [12 exercises with solution]/Exercise12.java
@@ -1,1 +1,21 @@
 Write a Java program to get a collection view of the values contained in this map.
+import java.util.*;
+
+public class MapValueCollection {
+    public static void main(String[] args) {
+        // Create a map
+        Map<String, Integer> map = new HashMap<>();
+        map.put("apple", 1);
+        map.put("banana", 2);
+        map.put("orange", 3);
+        
+        // Get a collection view of the values
+        Collection<Integer> values = map.values();
+        
+        // Print the values
+        System.out.println("Values in the map:");
+        for (Integer value : values) {
+            System.out.println(value);
+        }
+    }
+}
