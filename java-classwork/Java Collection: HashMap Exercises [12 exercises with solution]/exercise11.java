a77d7e5736d7b941f348da3f0b62e24e8d13de06
benjaminmillertx <156035893+benjaminmillertx@users.noreply.github.com>
2024-05-14 12:45:54 -0500

    Update exercise11.java

--- java-classwork/Java Collection: HashMap Exercises [12 exercises with solution]/exercise11.java
+++ java-classwork/Java Collection: HashMap Exercises [12 exercises with solution]/exercise11.java
@@ -1,1 +1,19 @@
 Write a Java program to get a set view of the keys contained in this map.
+import java.util.*;
+
+public class MapKeySetExample {
+    public static void main(String[] args) {
+        // Create a map
+        Map<String, Integer> map = new HashMap<>();
+        
+        // Add some key-value pairs
+        map.put("apple", 10);
+        map.put("banana", 20);
+        map.put("orange", 30);
+        
+        // Get the set view of keys
+        Set<String> keySet = map.keySet();
+        
+        // Print the key set
+        System.out.println("Keys: " + keySet);
+    }
