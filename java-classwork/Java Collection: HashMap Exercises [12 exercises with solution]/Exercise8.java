e6cb0e866bdece6bb2638a7e1a55e94dd4de267c
benjaminmillertx <156035893+benjaminmillertx@users.noreply.github.com>
2024-05-14 12:28:51 -0500

    Update Exercise8.java

--- java-classwork/Java Collection: HashMap Exercises [12 exercises with solution]/Exercise8.java
+++ java-classwork/Java Collection: HashMap Exercises [12 exercises with solution]/Exercise8.java
@@ -1,1 +1,29 @@
 Write a Java program to test if a map contains a mapping for the specified value.
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapContainsValue {
+    public static void main(String[] args) {
+        // Create a HashMap
+        Map<Integer, String> map = new HashMap<>();
+
+        // Add some key-value pairs
+        map.put(1, "One");
+        map.put(2, "Two");
+        map.put(3, "Three");
+        map.put(4, "Four");
+
+        // Specify the value to test
+        String valueToCheck = "Three";
+
+        // Check if the map contains the specified value
+        boolean containsValue = map.containsValue(valueToCheck);
+
+        // Print the result
+        if (containsValue) {
+            System.out.println("The map contains the value: " + valueToCheck);
+        } else {
+            System.out.println("The map does not contain the value: " + valueToCheck);
+        }
+    }
+}
