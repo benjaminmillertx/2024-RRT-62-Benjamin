8be98e1b6e52e22938a58b214fc4e20f46a62899
benjaminmillertx <156035893+benjaminmillertx@users.noreply.github.com>
2024-05-14 10:56:56 -0500

    Update exercise3.java

--- java-classwork/Java Collection: HashMap Exercises [12 exercises with solution]/exercise3.java
+++ java-classwork/Java Collection: HashMap Exercises [12 exercises with solution]/exercise3.java
@@ -1,1 +1,21 @@
-Write a Java program to copy all mappings from the specified map to another map.
+Write a Java program to copy all mappings from the specified map to another import java.util.HashMap;
+import java.util.Map;
+
+public class MapCopyExample {
+    public static void main(String[] args) {
+        // Creating the first map
+        Map<String, Integer> originalMap = new HashMap<>();
+        originalMap.put("A", 1);
+        originalMap.put("B", 2);
+        originalMap.put("C", 3);
+
+        // Creating the second map
+        Map<String, Integer> copiedMap = new HashMap<>();
+
+        // Copying mappings from originalMap to copiedMap
+        copiedMap.putAll(originalMap);
+
+        // Displaying the contents of copiedMap
+        System.out.println("Copied Map: " + copiedMap);
+    }
+}
