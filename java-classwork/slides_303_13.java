15da01c56d6722df82b757fd82d80c422d496e6b
benjaminmillertx <156035893+benjaminmillertx@users.noreply.github.com>
2024-05-16 11:03:14 -0500

    Create slides_303_13.java

---
+++ java-classwork/slides_303_13.java
@@ -0,0 +1,25 @@
+package org.sample.Slides_303_13
+
+import java.io.File:
+
+public class FileExample{
+
+File f = new file ("..\\.."):
+System.out.println*f.listFiles()
+for (FIle file:)}
+System.out.println(F.getName());
+
+File[] files = f.listFiles();
+for ( File file : file} {
+System.out.print(" " + file.getCanonicalPAth();
+if ( file.isDirectory()) {
+    System.out .println("is a directory")
+    System.out.println("is a file");
+}else {
+  }
+}
+
+file file =new file(postname".\\newfile.txt");
+file.createNewFile();
+
+}catch(
