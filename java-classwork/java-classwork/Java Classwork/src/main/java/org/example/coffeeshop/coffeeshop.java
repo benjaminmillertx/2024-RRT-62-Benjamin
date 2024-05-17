1a54210191c440117b21d16958674d1fe3688cf4
benjaminmillertx <156035893+benjaminmillertx@users.noreply.github.com>
2024-05-17 14:22:24 -0400

    Update coffeeshop.java

--- java-classwork/java-classwork/Java Classwork/src/main/java/org/example/coffeeshop/coffeeshop.java
+++ java-classwork/java-classwork/Java Classwork/src/main/java/org/example/coffeeshop/coffeeshop.java
@@ -1,40 +1,25 @@
-package org.example.coffeeshop;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CoffeeShop {
 
-    private List<Product> products = new ArrayList<>();
+@@ -37,11 +38,28 @@ public void initProducts() {
 
-    // building the menu of products for sale at the coffee shop
-    public void initProducts() {
-        ProductReader pr = new ProductReader();
-        List<Product> productsFromFile = pr.readProducts();
-        products.addAll(productsFromFile);
+    }
 
-        Product p1 = new Product();
-        p1.setPrice(5.46);
-        p1.setName("Small Coffee");
-        p1.setBeverage(true);
-        products.add(p1);
+    public void printCheapProducts() {
+        // this code filters all products that are less than 5$
+        List<Product> cheapProducts = products.stream().filter(product -> product.getPrice() < 5).collect(Collectors.toList());
+        // this code prints each of the products that is less than 5$
+        cheapProducts.forEach(product -> System.out.println(product.getName() + "\t" + product.getPrice()));
 
-        Product largeCoffee = new Product();
-        largeCoffee.setPrice(9.46);
-        largeCoffee.setName("Large Coffee");
-        largeCoffee.setBeverage(true);
-        products.add(largeCoffee);
-
-        Product cakePop = new Product();
-        cakePop.setPrice(4.89);
-        cakePop.setName("Cake Pop");
-        cakePop.setBeverage(false);
-        products.add(cakePop);
-
-        Product eggSandwich = new Product(7.89, "Egg Sandwich", false, 50);
-        products.add(eggSandwich);
-
-
+        // the same 2 lines of code written as a for loop
+        for ( Product product : products ) {
+            if ( product.getPrice() < 5 ) {
+                System.out.println(product.getName() + "\t" + product.getPrice());
+            }
+        }
     }
 
     // print the list of products available for sale
@@ -43,6 +28,8 @@ public class CoffeeShop {
             Product product = products.get(pos);
             System.out.println((pos+1) + ") " + product.getName() + "\t" + product.getPrice() );
         }
-        products.ForEach. 
+
+
+        //products.forEach( (product) -> {System.out.println(product.getName() + "\t" + product.getPrice() );});
     }
 }
