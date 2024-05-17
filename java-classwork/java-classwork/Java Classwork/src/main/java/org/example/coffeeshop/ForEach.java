d69d83fc962c8f1170f850ddb11a4dc73591c561
benjaminmillertx <156035893+benjaminmillertx@users.noreply.github.com>
2024-05-17 13:28:01 -0400

    Create ForEach.java

---
+++ java-classwork/java-classwork/Java Classwork/src/main/java/org/example/coffeeshop/ForEach.java
@@ -0,0 +1,29 @@
+import.java.Util.List
+
+public class ForEachExample {
+
+public static void main(String[] args) {
+List<String> names = new <ArrayList> string ();
+
+names.add("larry");
+names.add("steve");
+names.add("James");
+names.add("Conan");
+names.add("Ellen");
+
+names.forEach(name -> {
+]}- 
+
+System.out.println("-----------------------------------")
+
+for(String name: names)
+system.out.println("------------------------------------")
+names.stream().filter(name -> namestartsWith("E).collect.(Collectors.toList();
+
+for (string name: names) {
+system.out.println(name);
+  }
+}
+
+
+
